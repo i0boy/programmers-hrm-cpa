408a3576d55008c55c50cca6c076b9335319a38b
hyeoki <dlagogur93@gmail.com>
2023-02-12 06:32:16 +0900

    카드에 3개씩 인피니티 스크롤 적용

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -11,18 +11,57 @@ class CardView {
     this.$main = $main;
   }
 
+  infiniteScroll(/**@type{HTMLElement}*/ container) {
+    const target = () =>
+      document.getElementById("cards_container").lastElementChild;
+
+    console.log(document.getElementById("cards_container"));
+    const personalInfo = getPersonalInfo();
+    const io = new IntersectionObserver(
+      (entry) => {
+        if (entry[0].isIntersecting) {
+          const currentCardsCount =
+            document.getElementById("cards_container").childElementCount;
+          if (currentCardsCount === personalInfo.length) {
+            io.unobserve(target());
+            return;
+          }
+          Array.from({
+            length: Math.min(3, personalInfo.length - currentCardsCount),
+          }).forEach((_, i) => {
+            const card = cardDiv(i + currentCardsCount + "");
+            card.appendChild(
+              cardPlane("front", personalInfo[i + currentCardsCount].nickname)
+            );
+            card.appendChild(
+              cardPlane("back", personalInfo[i + currentCardsCount].mbti)
+            );
+            container.appendChild(card);
+          });
+        }
+        io.unobserve(target());
+        io.observe(target());
+      },
+      {
+        threshold: 0.7,
+      }
+    );
+    io.observe(target());
+  }
+
   render() {
     const containerDiv = document.createElement("div");
     containerDiv.setAttribute("id", "cards_container");
+
     const personalInfo = getPersonalInfo();
-    personalInfo.forEach((cardData, i) => {
+    personalInfo.slice(0, 3).forEach((cardData, i) => {
       const card = cardDiv(i + "");
       card.appendChild(cardPlane("front", cardData.nickname));
       card.appendChild(cardPlane("back", cardData.mbti));
       containerDiv.appendChild(card);
     });
-
     this.$main.appendChild(containerDiv);
+    this.infiniteScroll(containerDiv);
   }
 }
 export default CardView;
